bed133e9a2095d47f7c9f3d4e46708c23e98de8c
Umesh-01 <83420185+Umesh-01@users.noreply.github.com>
2021-05-10 00:58:52 +0530

    Update 01list_tup_exercise.py

--- 01list_tup_exercise.py
+++ 01list_tup_exercise.py
@@ -33,7 +33,117 @@ list = ['Python', 'Artificial Intelligence', 'Machine Learning', 'Deep Learning'
 for k in list:
     print('One item in my list is ' + k)
 
-# ------------------------------------------------
+# working list
+list = ['programmer', 'data analyst', 'app developer', 'full stack developer']
+print(list.index('app developer'))
+value = 'programmer'
+print(value in list)
+list.append('debugger')
+list.insert(0, 'dba')
+for i in list:
+    print(i)
+
+# starting from empty
+list = []
+list.append('programmer')
+list.append('data analyst')
+list.append('app developer')
+list.append('full stack developer')
+list.append('debugger')
+list.append('DBA')
+print('First career you thought of was- '+list[0])
+print('Last career you thought of was- '+list[len(list)-1])
+
+
+# ordered working list
+list = ['programmer', 'data analyst', 'app developer', 'full stack developer']
+
+print('the list in its original order.')
+for item in list:
+    print(item)
+print('the list in alphabetical order.')
+for item in sorted(list):
+    print(item)
+
+print('the list in its original order.')
+for item in list:
+    print(item)
+print('the list in its reverse alphabetical order.')
+for item in sorted(list, reverse=True):
+    print(item)
+
+print('the list in its original order.')
+for item in list:
+    print(item)
+print('the list in its reverse order from what it started.')
+list.reverse()
+for item in list:
+    print(item)
+
+print('the list in its original order.')
+list.reverse()
+for item in list:
+    print(item)
+print('Permanently sorting the list in alphabetical order.')
+list.sort()
+for item in list:
+    print(item)
+print('Permanently sorting the list in reverse alphabetical order.')
+list.sort(reverse=True)
+for item in list:
+    print(item)
+
+    
+# ordered numbers
+list = [34, 67, 21, 98, 11]
+
+print('the numbers in the original order.')
+for item in list:
+    print(item)
+print('the numbers the increasing order.')
+for item in sorted(list):
+    print(item)
+
+print('the numbers in the original order.')
+for item in list:
+    print(item)
+print('the numbers in the decreasing order.')
+for item in sorted(list, reverse=True):
+    print(item)
+
+print('the numbers in the original order.')
+for item in list:
+    print(item)
+print('the numbers in the reverse order from what they started.')
+list.reverse()
+for item in list:
+    print(item)
+
+print('the numbers in the original order.')
+list.reverse()
+for item in list:
+    print(item)
+print('Permanently sorting the numbers in increasing order.')
+list.sort()
+for item in list:
+    print(item)
+print('Permanently sorting the numbers in decreasing order.')
+list.sort(reverse=True)
+for item in list:
+    print(item)
+
+
+# list lengths
+list1 = ['Umesh', 'Singh', 'Learning', 'Python', 'Programming']
+list2 = [34, 67, 21, 98, 11, 55, 21]
+list3 = ['programmer', 'data analyst', 'app developer', 'full stack developer', 'debugger', 'DBA']
+list4 = ['Python', 'Artificial Intelligence', 51, 'Machine Learning', 21, 'Deep Learning']
+
+print('Items in list1- ', len(list1))
+print('Items in list2- ', len(list2))
+print('Items in list3- ', len(list3))
+print('Items in list4- ', len(list4))
+
 
 # famous people
 people = ['Elon Musk', 'Bill Gates', 'Jeff Bezos', 'Steve Jobs']
